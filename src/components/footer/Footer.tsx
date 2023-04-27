2780580c785482cf13f25dd5e2963e9ed1e5ffeb
Doukhaev Israil <doukahevisrail@MacBook-Air-de-Doukahev.local>
2023-04-27 16:29:03 +0200

    modify email

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -8,8 +8,7 @@ function Footer() {
       <div className={styles.footer_container}>
         <a href='https://www.linkedin.com/in/israil-doukhaev-61a41a218' className={styles.footer_icon}><img src="/linkedin.png" alt="icon of linkedin" className={styles.footer_icon}/></a>
         <a href='https://github.com/TheTrikkster/porfolio' className={styles.footer_icon}><img src="/github.png" alt="icon of github" className={styles.footer_icon} /></a>
-        {/* <a href='https://github.com/TheTrikkster/porfolio'><img src="/Humaaans.png" alt="icon of github" style={{width: "400px"}}/></a> */}
-        <p>israilst67@gmail.com</p>
+        <p><a href='mailto:israilst67@gmail.com' className={styles.footer_email}>israilst67@gmail.com</a></p>
       </div>
     </footer>
   )
